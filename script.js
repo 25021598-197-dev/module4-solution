1b39f74abac4c50a2d81900283229e6f75226513
25021598-197-dev <25021598-197@uog.edu.pk>
2026-09-13 21:02:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+
 (function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+var output = ""; // naya line
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
+    output = output + "Good Bye " + names[i] + "\n"; // naya line
   } else {
     helloSpeaker.speak(names[i]);
+    output = output + "Hello " + names[i] + "\n"; // naya line
   }
 }
+
+alert(output); // ye sabse important line
 })();
